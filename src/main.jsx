015cc49fe9397e05606e5fa4175a7f55007f0665
Frankjr43A <francisjunior82@gmail.com>
2026-07-01 16:43:32 +0300

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import "./index.css";
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register(
+        `${import.meta.env.BASE_URL}sw.js`
+      )
       .then(() => {
         console.log(
           "Service Worker Registered"
@@ -27,7 +29,9 @@ ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      basename={import.meta.env.BASE_URL}
+    >
       <App />
     </BrowserRouter>
   </React.StrictMode>
