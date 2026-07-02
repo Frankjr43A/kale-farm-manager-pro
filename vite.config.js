fbb9f33524fe2cb49b25483eb79104adb4fd3be2
Frankjr43A <francisjunior82@gmail.com>
2026-07-02 14:25:36 +0300

    Fix Android white screen by updating Vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/kale-farm-manager-pro/",
+  base:
+    process.env.NODE_ENV ===
+    "production"
+      ? "./"
+      : "/",
   plugins: [react()],
 });
